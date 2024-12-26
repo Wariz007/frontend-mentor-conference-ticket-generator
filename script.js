b472c464e4fdcceebda0aac3bdf3f7a73c3c5a5a
Abdulwariz <bookbodmas31@gmail.com>
2024-12-26 17:53:28 +0100

    email and fullname validation done

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const fullNameContainer = document.getElementById('full-name');
 const inputs = document.querySelectorAll('#input-container input');
 const fullNameErrorMessage = document.getElementById('fullname-error-message');
 
+const emailContainer = document.getElementById('email-address');
+const emailErrorMessage = document.getElementById('email-error-message');
+
+
 //create a function expression to contain the logic
 const validateFullName = () => {
     const fullname = fullNameContainer.value.trim();
@@ -34,19 +38,52 @@ const validateFullName = () => {
 
     return isValid;
 }
+const validateEmail = () => {
+    const email = emailContainer.value.trim();
+    let isValid = true;
+
+    //make sure email container is not empty
+    if(email === ''){
+        emailContainer.style.borderColor = 'red';
+        emailErrorMessage.classList.add('show');
+        isValid = false;
+    } else {
+        emailContainer.classList.remove('show');
+    }
+
+    //email regex
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if(!email.match(emailRegex)){
+        emailErrorMessage.classList.add('show');
+        emailContainer.style.borderColor = 'red';
+        isValid = false;
+    } else {
+        emailErrorMessage.classList.remove('show');
+    }
+
+    if(isValid){
+        emailContainer.style.borderColor = '';
+    }
+
+    return isValid;
+
+
+}
 
 //activate button
 generateTicketBtn.addEventListener('click', (event) => {
     //assign function expression to variable 
     const isFullNameValid = validateFullName();
+    const isEmailValid = validateEmail();
 
-    if(isFullNameValid){
-        console.log('validation successful', isFullNameValid);
+    if(isFullNameValid &&
+        isEmailValid) {
+        console.log('validation successful');
         setTimeout(() => {
             inputs.forEach(input => input.value = '')
         }, 3000);
     } else {
         event.preventDefault();
-        console.log('validation unsuccessful', isFullNameValid);
+        console.log('validation unsuccessful');
     }
 })
